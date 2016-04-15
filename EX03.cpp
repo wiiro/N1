5ac5059531bb95348f9c21e79aee485eccea3513
wiiro <willianrodrigues.lima@gmail.com>
2016-04-15 15:25:50 -0300

    Added files via upload

--- EX03.cpp
+++ EX03.cpp
@@ -22,9 +22,6 @@ using namespace std;
 #define DOWN   	80
 #define ESC    	27
 
- void desenhaNave();
- void moveNave();
-
  //paramentros do putpixel
  typedef struct {
 	int x; 
@@ -44,55 +41,56 @@ typedef struct{
 	TMove passo;
  }TNave;
 
+ void printNave(TNave *n);
+ void printFundo(TEstrelas *stars);
 
 int main(){
- //declaracao de variaveis
- char tecla;
  
- initwindow(800, 600, "N1 - EX 03");						// inicio da parte grafica	
- cleardevice();												// garantir que a tela está limpa para desenhar
- srand (time(NULL));										//seed do rand
- 	
- TEstrelas	Estrelas[2000];									//vetor para gerar 2000 estrelas coloridas
- TNave		move;		
-
-  //while(tecla != ESC){
-  //laço para gerar as posições aleatorias e guardá-las no vetor
-   for( int i = 0; i < 2000; i++){			
-   Estrelas[i].cor = rand () % 15 + 1;						//gerar cores aleatorias
-   Estrelas[i].x = rand () % 799;							//coordenada x aleatoria
-   Estrelas[i].y = rand () % 599;							//coordenada y aleatoria
-   putpixel(Estrelas[i].x, Estrelas[i].y, Estrelas[i].cor);	//desenho aleatorio
-   }
-
- desenhaNave();											
- moveNave();
+ initwindow(800, 600, "N1 - EX 03");									//Inicio da parte grafica	
+ cleardevice();															//Garantir que a tela está limpa para desenhar
+ srand (time(NULL));													//seed do rand
+ 
+ //declaracao de variaveis
+ char tecla;	
+ TEstrelas	Estrelas[2000];												//vetor para gerar 2000 estrelas coloridas
+ TNave		move;														//variavel da posicao da nave
+ int 		pg = 1;
+ int		i = 1;
+ move.passo.passo_x = 410;												//posicao inicial da nave em x
+ move.passo.passo_y = 380;												//posicao inicial da nave em y
+ 
+ //printFundo(Estrelas);
+ 
+  for(i = 0; i < 2000; i++){			
+   Estrelas[i].cor = rand () % 15 + 1;									//gerar cores aleatorias
+   Estrelas[i].x = rand () % 799;										//coordenada x aleatoria
+   Estrelas[i].y = rand () % 599;										//coordenada y aleatoria
+   putpixel(Estrelas[i].x, Estrelas[i].y, Estrelas[i].cor);				//desenho aleatorio
+   }		
   
+ while(tecla != ESC){
+	if (pg == 1) pg = 2; else pg = 1;
+	setactivepage(pg);													//Pagina ativa
+	putpixel(Estrelas[i].x, Estrelas[i].y, Estrelas[i].cor);			//desenho das estrelas
+	cleardevice();														//Limpa tela
+	printNave(&move);													//Desenha Nave
+	
+    if (kbhit()){
+    tecla = getch();
+	if(tecla == LEFT) 	move.passo.passo_x	-= 2;						// Movimenta para esquerda em X
+ 	if(tecla == RIGHT)	move.passo.passo_x 	+= 2;						// Movimenta para direita em X
+ 	if(tecla == UP) 	move.passo.passo_y	-= 1;						// Movimenta para cima em Y
+ 	if(tecla == DOWN) 	move.passo.passo_y	+= 1;						// Movimenta para baixo em Y								
+	printNave(&move);
+ 	}
+ 	setvisualpage(pg);													//pagina mostrada ao usuário
+ }
+
 getch();
 }
 
-
- void desenhaNave(){
- 	
-  int nave [8]; 
-  int nave_cor; 
-	
-  nave[0] = 410;
-  nave[1] = 380;
-  nave[2] = 380;
-  nave[3] = 450;
-  nave[4] = 440;
-  nave[5] = 450;
-  nave[6] = 410;
-  nave[7] = 380;
-  nave_cor = RGB(rand() % 200, rand() % 156, rand() % 200); 
-  setfillstyle(1, nave_cor);		
-  setcolor(YELLOW);
-  fillpoly(4, nave);
- 	
- } 
  
- void moveNave (TNave *n){
+ void printNave (TNave *n){
 	
 	int nave [8]; 
   	int nave_cor; 
@@ -100,11 +98,11 @@ getch();
 	nave[0] = n->passo.passo_x;
 	nave[1] = n->passo.passo_y;
 
-	nave[2] = n->passo.passo_x + 20;
-	nave[3] = n->passo.passo_y + 12;
+	nave[2] = n->passo.passo_x - 40;
+	nave[3] = n->passo.passo_y + 70;
 
-	nave[4] = n->passo.passo_x;
-	nave[5] = n->passo.passo_y + 24;
+	nave[4] = n->passo.passo_x + 40;
+	nave[5] = n->passo.passo_y + 70;
 
 	nave[6] = n->passo.passo_x;
 	nave[7] = n->passo.passo_y;
@@ -114,4 +112,13 @@ getch();
   	setcolor(YELLOW);
   	fillpoly(4, nave);
 }
+
+/*void printFundo(TEstrelas stars[]){
+ for( int i = 0; i < 2000; i++){			
+   stars[i].cor = rand () % 15 + 1;									//gerar cores aleatorias
+   stars[i].x = rand () % 799;										//coordenada x aleatoria
+   stars[i].y = rand () % 599;										//coordenada y aleatoria
+   putpixel(stars[i].x, stars[i].y, stars[i].cor);					//desenho aleatorio
+   }*/		
+
  
